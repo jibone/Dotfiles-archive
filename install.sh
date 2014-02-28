4e91fe56c3be48e128c46be6be5a21acf4f57a0c
J Shamsul Bahri <jibone@gmail.com>
2014-02-28 19:15:37 +0800

    bash dot files added.

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# Install script will copy dot files to the right place.
+# And backup previous files
+
+mkdir backup
+cp ~/.bashrc backup/bashrc
+rm ~/.bachrc
+ln -s files/bashrc ~/.bashrc
+cp ~/.bash_profile backup/bash_profile
+rm ~/.bash_profile
+ln -s files/bash_profile ~/.bash_profile
+
