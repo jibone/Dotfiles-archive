ce7b28c6ffe131ddc811bf98ff889ff9c5c5decd
J Shamsul Bahri <jibone@gmail.com>
2014-03-01 02:24:24 +0800

    add git and vim dot files

--- rollback.sh
+++ rollback.sh
@@ -3,8 +3,24 @@
 # Restore backup files.
 #
 
+echo "restore bash"
 rm ~/.bashrc
 cp backup/bashrc ~/.bashrc
 rm ~/.bash_profile
 cp backup/bash_profile ~/.bash_profile
 
+echo "restore git"
+rm ~/.git-completion.bash
+cp backup/git-completion.bash ~/.git-completion.bash
+rm ~/.git-prompt.sh
+cp backup/git-prompt.sh ~/.git-prompt.sh
+rm ~/.gitignore_global
+cp backup/gitignore_global ~/.gitignore_global
+
+echo "restore git"
+rm ~/.vimrc
+cp backup/vimrc ~/.vimrc
+rm ~/.viminfo
+cp backup/viminfo ~/.viminfo
+rm ~/.vim
+cp -r backup/vim ~/.vim
