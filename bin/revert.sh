782d651c3b278d939a280c3da67b404eb7dd291c
J Shamsul Bahri <jibone@gmail.com>
2014-12-28 02:49:12 +0800

    Fix revert and add backup folder to repo

--- bin/revert.sh
+++ bin/revert.sh
@@ -5,37 +5,35 @@
 # This file reverts the changes made when running setup.sh
 # ---------------------------------------------------------------
 
-set -e
-
 # Reverts bash_profile and bashrc -------------------------------
 if [ -f ../backup/bash/bash_profile ]; then
   echo "Revert bash_profile from backup..."
-  mv ../backup/bash/bash_profile ~/.bash_profile
+  mv ~/Dotfiles/backup/bash/bash_profile ~/.bash_profile
 fi
 
 if [ -f ../backup/bash/bashrc ]; then
   echo "Revert bashrc from backup..."
-  mv ../backup/bash/bashrc ~/.bashrc
+  mv ~/Dotfiles/backup/bash/bashrc ~/.bashrc
 fi
 
 # Reverts git prompt and git completion -------------------------
 if [ -f ../backup/git/git-completion.bash ]; then
   echo "Revert git-completion.bash from backup..."
-  mv ../backup/git/git-completion.bash ~/.git-completion.bash
+  mv ~/Dotfiles/backup/git/git-completion.bash ~/.git-completion.bash
 fi
 
 if [ -f ../backup/git/git-prompt.sh ]; then
   echo "Revert git-prompt.sh from backup..."
-  mv ../backup/git/git-prompt.sh ~/.git-prompt.sh
+  mv ~/Dotfiles/backup/git/git-prompt.sh ~/.git-prompt.sh
 fi
 
 # Revert vimrc and vim folder -----------------------------------
 if [ -d ../backup/vim/vim ]; then
   echo "Revert vim folder from backup..."
-  mv ../backup/vim/vim ~/.vim
+  mv ~/Dotfiles/backup/vim/vim ~/.vim
 fi
 
 if [ -f ../backup/vim/vimrc ]; then
   echo "Revert vimrc from backup..."
-  mv ../backup/vim/vimrc ~/.vimrc
+  mv ~/Dotfiles/backup/vim/vimrc ~/.vimrc
 fi
