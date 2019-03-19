c7b95af418a2a7cf04ab156fd463e3f228805fc0
J Shamsul Bahri (jibone)) <jibone@gmail.com>
2019-03-19 23:52:26 +0800

    Made some changes for dot files to work on Mav Mini
    Exit the battery.sh script
    
    Add note to bre install reattach-to-user-namespace. I forgot this step
    and tmux was not working for me
    
    TODO: maybe add into setup script to do install homebrew and do all the
    needed brew install

--- bash/scripts/battery.sh
+++ bash/scripts/battery.sh
@@ -9,9 +9,15 @@ PLUG='⚡'
 if [[ `uname` == 'Darwin' ]]; then
   # TODO figure out not calling battery_info 3 times....
   battery_info=`ioreg -rc AppleSmartBattery`
-  current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
-  total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
-  external_connected=$(echo $battery_info | grep -o '"ExternalConnected" = [a-zA-Z]\+' | awk '{print $3}')
+  if [ -z "$battery_info" ]
+  then
+    # exit this script when there is no battery. (this is for my Mac Mini)
+    exit 0
+  else
+    current_charge=$(echo $battery_info | grep -o '"CurrentCapacity" = [0-9]\+' | awk '{print $3}')
+    total_charge=$(echo $battery_info | grep -o '"MaxCapacity" = [0-9]\+' | awk '{print $3}')
+    external_connected=$(echo $battery_info | grep -o '"ExternalConnected" = [a-zA-Z]\+' | awk '{print $3}')
+  fi
 else
   # Script currently only works for MacOSX
   exit 0
@@ -19,7 +25,7 @@ fi
 
 
 if [[ $external_connected == 'Yes' ]]; then
-  echo -n "$PLUG  "
+  echo -n "$PLUG "
 fi
 
 charged_slots=$(echo "(($current_charge/$total_charge)*10)+1" | bc -l | cut -d '.' -f 1)
